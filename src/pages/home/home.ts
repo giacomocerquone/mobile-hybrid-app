03a817ba290ad71d14f29834eaa7fe114167ece2
Giacomo Cerquone <cerquone@gmail.com>
2018-03-11 19:38:02 +0100

    added sidebar navigation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, MenuController } from 'ionic-angular';
 import { Http } from '@angular/http';
 
 import 'rxjs/add/operator/map';
@@ -14,11 +14,15 @@ export class HomePage {
 
   public Drink: Drink;
 
-  constructor(public navCtrl: NavController, public _http: Http) {
+  constructor(public navCtrl: NavController, public _http: Http, public _menuController: MenuController) {
     this._http.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka')
       .map(res => res.json())
       .subscribe(data => {
         this.Drink = new Drink(data.ingredients[0].strIngredient, data.ingredients[0].strDescription);
       })
+  }
+
+  openMenu() {
+    this._menuController.open();
   }
 }
