7222410ed09aed6937750990e43165f7257d0fec
Giacomo Cerquone <cerquone@gmail.com>
2018-05-31 19:25:00 +0200

    Mocking persons with ts file, shaped up Person Model

---
+++ src/mock/persons.ts
@@ -0,0 +1,29 @@
+import { Person } from "../models/Person";
+
+export const Persons: Array<Person> = [
+  {
+    name: 'Paolo',
+    surname: 'Della Penna',
+    imageProfile: 'http://americanmuslimconsumer.com/2013/wp-content/uploads/2013/09/blank-user.jpg',
+  },  {
+    name: 'Paolo',
+    surname: 'Della Penna',
+    imageProfile: 'http://americanmuslimconsumer.com/2013/wp-content/uploads/2013/09/blank-user.jpg',
+  },  {
+    name: 'Paolo',
+    surname: 'Della Penna',
+    imageProfile: 'http://americanmuslimconsumer.com/2013/wp-content/uploads/2013/09/blank-user.jpg',
+  },  {
+    name: 'Paolo',
+    surname: 'Della Penna',
+    imageProfile: 'http://americanmuslimconsumer.com/2013/wp-content/uploads/2013/09/blank-user.jpg',
+  },  {
+    name: 'Paolo',
+    surname: 'Della Penna',
+    imageProfile: 'http://americanmuslimconsumer.com/2013/wp-content/uploads/2013/09/blank-user.jpg',
+  },  {
+    name: 'Paolo',
+    surname: 'Della Penna',
+    imageProfile: 'http://americanmuslimconsumer.com/2013/wp-content/uploads/2013/09/blank-user.jpg',
+  },
+];
