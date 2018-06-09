02426ac6868578de27d6b54a88d4df62af342f5c
Giacomo Cerquone <cerquone@gmail.com>
2018-06-09 15:00:02 +0200

    Implemented Get Locals, improved conditional rendering of avatars

--- src/providers/local-service/local-service.ts
+++ src/providers/local-service/local-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Person } from '../../models/Person';
+import { Local } from '../../models/Local';
 
 @Injectable()
 export class LocalServiceProvider {
@@ -8,13 +8,13 @@ export class LocalServiceProvider {
   constructor(public http: HttpClient) {}
 
   public getLocals() {
-    return this.http.get<Person[]>('Locals');
+    return this.http.get<Local[]>('Locals');
   }
 
   public searchLocal(searchParams) {
     const params = new HttpParams();
     Object.keys(searchParams).forEach(el => params.append(el, params[el]));
-    return this.http.get<Person[]>('Locals/findOne', { params });
+    return this.http.get<Local[]>('Locals/findOne', { params });
   }
 
 }
